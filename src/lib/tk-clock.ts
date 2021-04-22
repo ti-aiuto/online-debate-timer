7cdf54778446b2b82dd8fe97d5f719920542d6d0
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2021-04-22 20:13:50 +0900

    :heavy_plus_sign: tk-clock定義

---
+++ src/lib/tk-clock.ts
@@ -0,0 +1,48 @@
+type TickCallbackType = () => void;
+
+const SYSTEM_INTERVAL = 50;
+
+// 経過時間を厳密に測るため時刻で比較するclock
+export class TkClock {
+  private timerId: number | null = null;
+  private tickStartedAtMsec: number = 0;
+  private lastTimeTickedSec: number = 0;
+  private tickCallback: TickCallbackType | null = null;
+
+  setTickCallback(callback: TickCallbackType) {
+    this.tickCallback = callback;
+  }
+
+  private notifyTick() {
+    if (this.tickCallback) {
+      this.tickCallback();
+    }
+  }
+
+  start() {
+    this.tickStartedAtMsec = Date.now()
+    this.timerId = setInterval(() => {
+      this.onIntervalTick()
+    }, SYSTEM_INTERVAL)
+  }
+
+  stop() {
+    if (this.timerId) {
+      clearInterval(this.timerId)
+    }
+    this.timerId = null
+    this.tickStartedAtMsec = 0
+    this.lastTimeTickedSec = 0
+  }
+
+  private onIntervalTick() {
+    // スタート時点の時刻と現在時刻を比較して正確な経過時間を計測する
+    const currentTime = Date.now()
+    const d = currentTime - this.tickStartedAtMsec
+    const timeTickedSec = Math.floor(d / 1000)
+    if (timeTickedSec !== this.lastTimeTickedSec) {
+      this.notifyTick()
+      this.lastTimeTickedSec = timeTickedSec
+    }
+  }
+}
