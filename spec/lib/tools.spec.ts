ba00df8d6f951b59a071ae40fcfd8a6f82a40a41
ti-aiuto <ti.aiuto@outlook.jp>
2021-04-22 23:22:55 +0900

    :white_check_mark: zeroPaddingのテスト追加

--- spec/lib/tools.spec.ts
+++ spec/lib/tools.spec.ts
@@ -1,4 +1,4 @@
-import { isPositiveInteger } from '@/lib/tools'
+import { isPositiveInteger, zeroPadding } from '@/lib/tools'
 
 describe('isPositiveInteger', () => {
   it('戻り値の確認', () => {
@@ -16,5 +16,15 @@ describe('isPositiveInteger', () => {
     expect(isPositiveInteger('1-')).toBe(false) // 正常な値に不正な値がついている場合
     expect(isPositiveInteger('21-')).toBe(false)
     expect(isPositiveInteger('１')).toBe(false) // 全角
+  })
+})
+
+describe('zeroPadding', () => {
+  it('戻り値の確認', () => {
+    expect(zeroPadding(0)).toEqual('00')
+    expect(zeroPadding(1)).toEqual('01')
+    expect(zeroPadding(99)).toEqual('99')
+    expect(zeroPadding(987)).toEqual('87')
+    expect(zeroPadding(-1)).toEqual('XX')
   })
 })
