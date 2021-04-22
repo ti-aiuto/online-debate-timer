f190b8331d6469223ad20ba6c732a155f32bc3c3
ti-aiuto <ti.aiuto@outlook.jp>
2021-04-22 23:22:46 +0900

    :pencil2: zeroPaddingの実装修正

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -2,6 +2,11 @@ export function isPositiveInteger(number: number | string): boolean {
   return !!`${number}`.match(/^\d+$/) && parseInt(`${number}`) > 0
 }
 
-export function zeroPadding(positiveInteger: number) {
-  return ('000' + positiveInteger).slice(-2)
+// とりあえず2桁の0埋めにだけ対応
+export function zeroPadding(integer: number): string {
+  if (integer === 0 || isPositiveInteger(integer)) {
+    return ('000' + integer).slice(-2)
+  }
+  // 不正な値はXXにしてしまう
+  return 'XX'
 }
