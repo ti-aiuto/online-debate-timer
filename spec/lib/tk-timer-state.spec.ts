cdf8aa4965c9d83a8f3c05f463d500d0c8e70d97
ti-aiuto <ti.aiuto@outlook.jp>
2021-04-22 23:59:29 +0900

    :white_check_mark: 現在の状態問い合わせのテストも追加

--- spec/lib/tk-timer-state.spec.ts
+++ spec/lib/tk-timer-state.spec.ts
@@ -5,26 +5,6 @@ describe('TkTimerState', () => {
     return new TkTimerState()
   }
 
-  describe('コールバックのテスト', () => {
-    describe('コールバックがセットされている場合', () => {
-      it('呼び出されること', () => {
-        const state = buildState()
-        const callback = jest.fn()
-        state.setStateChangedCallback(callback)
-
-        state.goToSettingTime()
-        expect(callback).toHaveBeenCalledWith()
-      })
-    })
-
-    describe('コールバックがセットされていない場合', () => {
-      it('エラーにならないこと', () => {
-        const state = buildState()
-        expect(() => state.goToSettingTime()).not.toThrowError()
-      })
-    })
-  })
-
   function buildInitialState() {
     const state = buildState()
     return state
@@ -54,10 +34,35 @@ describe('TkTimerState', () => {
     return state
   }
 
+  describe('コールバックのテスト', () => {
+    describe('コールバックがセットされている場合', () => {
+      it('呼び出されること', () => {
+        const state = buildState()
+        const callback = jest.fn()
+        state.setStateChangedCallback(callback)
+
+        state.goToSettingTime()
+        expect(callback).toHaveBeenCalledWith()
+      })
+    })
+
+    describe('コールバックがセットされていない場合', () => {
+      it('エラーにならないこと', () => {
+        const state = buildState()
+        expect(() => state.goToSettingTime()).not.toThrowError()
+      })
+    })
+  })
+
   describe('状態遷移問い合わせのテスト', () => {
     it('戻り値の確認', () => {
       let state = buildInitialState()
       expect(state.state).toEqual('INITIAL_STATE')
+      expect(state.isInitialState()).toBe(true)
+      expect(state.isSettingTime()).toBe(false)
+      expect(state.isCountingDown()).toBe(false)
+      expect(state.isPausingCountDown()).toBe(false)
+      expect(state.isCountingDownCompleted()).toBe(false)
       expect(state.canGoToInitialState()).toBe(true)
       expect(state.canGoToSettingTime()).toBe(true)
       expect(state.canGoToCountingDown()).toBe(false)
@@ -66,30 +71,50 @@ describe('TkTimerState', () => {
 
       state = buildSettingTime()
       expect(state.state).toEqual('SETTING_TIME')
+      expect(state.isInitialState()).toBe(false)
+      expect(state.isSettingTime()).toBe(true)
+      expect(state.isCountingDown()).toBe(false)
+      expect(state.isPausingCountDown()).toBe(false)
+      expect(state.isCountingDownCompleted()).toBe(false)
       expect(state.canGoToInitialState()).toBe(true)
       expect(state.canGoToSettingTime()).toBe(true)
       expect(state.canGoToCountingDown()).toBe(true)
       expect(state.canGoToPausingCountDown()).toBe(false)
       expect(state.canGoToCountingDownCompleted()).toBe(false)
-      
+
       state = buildCountingDown()
       expect(state.state).toEqual('COUNTING_DOWN')
+      expect(state.isInitialState()).toBe(false)
+      expect(state.isSettingTime()).toBe(false)
+      expect(state.isCountingDown()).toBe(true)
+      expect(state.isPausingCountDown()).toBe(false)
+      expect(state.isCountingDownCompleted()).toBe(false)
       expect(state.canGoToInitialState()).toBe(true)
       expect(state.canGoToSettingTime()).toBe(false)
       expect(state.canGoToCountingDown()).toBe(true)
       expect(state.canGoToPausingCountDown()).toBe(true)
       expect(state.canGoToCountingDownCompleted()).toBe(true)
-      
+
       state = buildPausingCountingDown()
       expect(state.state).toEqual('PAUSING_COUNTING_DOWN')
+      expect(state.isInitialState()).toBe(false)
+      expect(state.isSettingTime()).toBe(false)
+      expect(state.isCountingDown()).toBe(false)
+      expect(state.isPausingCountDown()).toBe(true)
+      expect(state.isCountingDownCompleted()).toBe(false)
       expect(state.canGoToInitialState()).toBe(true)
       expect(state.canGoToSettingTime()).toBe(false)
       expect(state.canGoToCountingDown()).toBe(true)
       expect(state.canGoToPausingCountDown()).toBe(true)
       expect(state.canGoToCountingDownCompleted()).toBe(false)
-      
+
       state = buildCountingDownCompleted()
       expect(state.state).toEqual('COUNTING_DOWN_COMPLETED')
+      expect(state.isInitialState()).toBe(false)
+      expect(state.isSettingTime()).toBe(false)
+      expect(state.isCountingDown()).toBe(false)
+      expect(state.isPausingCountDown()).toBe(false)
+      expect(state.isCountingDownCompleted()).toBe(true)
       expect(state.canGoToInitialState()).toBe(true)
       expect(state.canGoToSettingTime()).toBe(true)
       expect(state.canGoToCountingDown()).toBe(false)
