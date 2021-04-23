1038a449abdf38e6c73be172763f96194ca55c52
ti-aiuto <ti.aiuto@outlook.jp>
2021-04-23 09:33:30 +0900

    :pencil2: 実装変更

--- src/lib/time-up-audio.ts
+++ src/lib/time-up-audio.ts
@@ -1,14 +1,17 @@
-import { TkAudio } from "@/lib/tk-audio";
+import { TkAudio } from '@/lib/tk-audio'
 
 export class TimeUpAudio extends TkAudio {
   play() {
     this.setLoop(true)
-    super.play();
+    super.play()
   }
 
   testPlay() {
-    this.setCurrentTime(7.3);
-    this.setLoop(false);
-    this.audio.play();
+    if (this.isPlaying) {
+      return
+    }
+    this.setCurrentTime(7.3)
+    this.setLoop(false)
+    this.audio.play()
   }
 }
