8dd89cbded2ebc1c71910e6ece384b5e09e8bc28
ti-aiuto <ti.aiuto@outlook.jp>
2021-04-26 23:18:27 +0900

    :heavy_plus_sign: vue-jest入れた

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,13 @@ module.exports = {
   preset: 'ts-jest',
   moduleNameMapper: {
     '^@/(.+)': '<rootDir>/src/$1'
+  },
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue'
+  ],
+  transform: {
+    '.*\\.(vue)$': 'vue-jest'
   }
 }
