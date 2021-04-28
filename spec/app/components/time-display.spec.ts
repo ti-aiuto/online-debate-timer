55316c9cd3199eec6fec4f42f537efd9fb4cf5a1
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2021-04-28 20:30:33 +0900

    :white_check_mark: minutesのテスト追加

--- spec/app/components/time-display.spec.ts
+++ spec/app/components/time-display.spec.ts
@@ -1,16 +1,59 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import { ComponentWrapper, testMountComponent } from '../../utils'
 import TimeDisplay from '@/app/components/time-display.vue'
 
-function factory(): ComponentWrapper {
-  return mount(TimeDisplay, {
-    propsData: {
-      sec: 123,
-      soundEnabled: true
-    }
-  })
+function factory({
+  sec = 123,
+  soundEnabled = true,
+  shallow = true
+} = {}): ComponentWrapper {
+  if (shallow) {
+    return shallowMount(TimeDisplay, {
+      propsData: {
+        sec,
+        soundEnabled
+      }
+    })
+  } else {
+    return mount(TimeDisplay, {
+      propsData: {
+        sec,
+        soundEnabled
+      }
+    })
+  }
 }
 
 describe('TimeDisplay', () => {
-  testMountComponent(factory)
+  testMountComponent(() => factory({ shallow: false }))
+
+  describe('computed', () => {
+    describe('minutes', () => {
+      it('値の確認', () => {
+        let wrapper = factory({ sec: 0 })
+        expect(wrapper.vm.minutes).toBe('00')
+
+        wrapper = factory({ sec: 1 })
+        expect(wrapper.vm.minutes).toBe('00')
+
+        wrapper = factory({ sec: 59 })
+        expect(wrapper.vm.minutes).toBe('00')
+
+        wrapper = factory({ sec: 60 })
+        expect(wrapper.vm.minutes).toBe('01')
+
+        wrapper = factory({ sec: 119 })
+        expect(wrapper.vm.minutes).toBe('01')
+
+        wrapper = factory({ sec: 120 })
+        expect(wrapper.vm.minutes).toBe('02')
+
+        wrapper = factory({ sec: 60 * 99 })
+        expect(wrapper.vm.minutes).toBe('99')
+
+        wrapper = factory({ sec: 60 * 99 + 1 })
+        expect(wrapper.vm.minutes).toBe('99')
+      })
+    })
+  })
 })
