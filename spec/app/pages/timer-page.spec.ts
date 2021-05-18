228fc5850a74bbbf0f710761390517608ba8cd91
ti-aiuto <ti.aiuto@outlook.jp>
2021-05-18 22:36:41 +0900

    各methodsのテストも追加

--- spec/app/pages/timer-page.spec.ts
+++ spec/app/pages/timer-page.spec.ts
@@ -30,4 +30,37 @@ describe('TimerPage', () => {
       })
     })
   })
+
+  describe('methods', () => {
+    describe('plusScaleButton', () => {
+      it('scaleの値が変わること', () => {
+        const wrapper = factory()
+        wrapper.vm.scale = 1
+        wrapper.vm.plusScaleButton()
+        expect(wrapper.vm.scale).toBe(1 + 0.1)
+      })
+    })
+
+    describe('minusScaleButton', () => {
+      it('scaleの値が変わること', () => {
+        const wrapper = factory()
+        wrapper.vm.scale = 0.6
+        wrapper.vm.minusScaleButton()
+        expect(wrapper.vm.scale).toBe(0.6 - 0.1)
+
+        wrapper.vm.scale = 0.5
+        wrapper.vm.minusScaleButton()
+        expect(wrapper.vm.scale).toBe(0.5)
+      })
+    })
+
+    describe('resetScaleButton', () => {
+      it('scaleの値が変わること', () => {
+        const wrapper = factory()
+        wrapper.vm.scale = 2.5
+        wrapper.vm.resetScaleButton()
+        expect(wrapper.vm.scale).toBe(1)
+      })
+    })
+  })
 })
