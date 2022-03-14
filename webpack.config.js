6add3479cd95aba042338b367cf224ad170aacfb
ti-aiuto <ti.aiuto@outlook.jp>
2022-03-14 22:36:53 +0900

    :pencil2: 出力ファイル名変更

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'application-[chunkhash].js',
-    assetModuleFilename: '[hash][ext]'
+    assetModuleFilename: '[name]-[hash][ext]'
   },
   module: {
     rules: [
